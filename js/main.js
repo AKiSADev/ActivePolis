726a2781fdb26e733c194324cde7ff63c3f4d1c2
AKiSADev <vivianalamboni@gmail.com>
2020-02-08 00:27:17 +0100

    Mappa + post

--- js/main.js
+++ js/main.js
@@ -5,9 +5,13 @@ var userMail;
 var userPass;
 var map;
 
-$(document).ready(function () {
+var segnalazioni;
+
+$(document).ready(function() {
     console.log("ready!");
 
+
+    initMap();
 
 });
 
@@ -39,8 +43,7 @@ function validaLoginMock() {
         $('#entra').hide();
         $('#esci').show()
         document.getElementById("gestisci").hidden = true;
-    }
-    else {
+    } else {
         $('#wrongCred').show();
         document.getElementById("gestisci").hidden = true;
     }
@@ -106,13 +109,56 @@ function closeLateralMenu() {
 
 }
 
-$(function () {
-    $('.btn-group-fab').on('click', '.btn', function () {
+$(function() {
+    $('.btn-group-fab').on('click', '.btn', function() {
         $('.btn-group-fab').toggleClass('active');
         //TODO OPEN MODAL INSERIMENTO DELLA SEGNALAZIONE
     });
 });
 
-$(document).click(function(event) {
-    console.log($(event.target));
-});
+// $(document).click(function(event) {
+//     console.log($(event.target));
+// });
+
+
+function initMap() {
+    map = new OpenLayers.Map("map", {
+        projection: new OpenLayers.Projection("EPSG:900913"),
+        displayProjection: new OpenLayers.Projection("EPSG:4326")
+    });
+
+    var mapnik = new OpenLayers.Layer.OSM("OpenStreetMap (Mapnik)");
+
+    map.addLayer(mapnik);
+
+    var lonLat = new OpenLayers.LonLat(16.852, 41.1187)
+        .transform(
+            new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+            map.getProjectionObject() // to Spherical Mercator Projection
+        );
+
+    map.setCenter(lonLat, 13);
+
+    var LayerMarkers = new OpenLayers.Layer.Markers("Markers");
+    map.addLayer(LayerMarkers);
+
+}
+
+
+
+//LA SEGUENTE FUNZIONE SERVIRÀ A CERCA IL POSTO DELLA SEGNALAZIONE
+function getPosition(place) {
+    $.ajax({
+        url: "http://open.mapquestapi.com/geocoding/v1/address?key=ZOwWyHSPqFu1airrcvdG5d05XcfGTln0&location=" + place + ", BA",
+        success: function(result) {
+            console.log(result.results[0]);
+        }
+    });
+
+    //AGGIUNGI MARKER DI SEGUITO
+    // LayerMarkers.addMarker(new OpenLayers.Marker(LonLat));
+}
+
+// ZOwWyHSPqFu1airrcvdG5d05XcfGTln0
+
+// C2Nn3TEHOh28mjfr
